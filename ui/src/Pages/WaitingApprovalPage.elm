612bdb21fe98ec7026180a967d99f069fd6cf3cc
theNerd247 <noah.harvey247 gm>
2018-03-06 20:17:43 -0500

    refactored final page view

---
+++ ui/src/Pages/WaitingApprovalPage.elm
@@ -0,0 +1,64 @@
+module Pages.WaitingApprovalPage exposing (..)
+
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Grid.Row as Row
+import Data exposing (listPersonView)
+import FindPeople as Find
+import Html as Html exposing (Html, div, h1, h2, h3, h4, p, text)
+import Html.Attributes exposing (class, for, style)
+
+
+finishedPageView : Find.HasFind m -> (Find.Msg -> msg) -> Html msg
+finishedPageView mdl f =
+    let
+        title =
+            Grid.row [ Row.centerXs ]
+                [ Grid.col [ Col.xsAuto ]
+                    [ h2 [] [ text "You're Almost Done!" ]
+                    ]
+                ]
+
+        inst =
+            Grid.row [ Row.centerXs ]
+                [ Grid.col [ Col.xs10 ]
+                    [ p [ class "text-center" ]
+                        [ text "Stop by the check-in desk and show them this number"
+                        ]
+                    ]
+                ]
+
+        cancelCheckin =
+            Grid.row [ Row.centerXs, Row.attrs [ class "pb-3" ] ]
+                [ Grid.col [ Col.xsAuto ]
+                    [ Html.map f <| Find.cancelCheckInButton
+                    ]
+                ]
+
+        groupId gid =
+            Grid.row [ Row.centerXs ]
+                [ Grid.col [ Col.xsAuto ]
+                    [ h1 [] [ text <| toString gid ]
+                    ]
+                ]
+
+        checkInTitle =
+            Grid.row [ Row.centerXs ]
+                [ Grid.col [ Col.xs10 ]
+                    [ h3 [] [ text "You're Checking In" ]
+                    ]
+                ]
+    in
+    div [] <|
+        case mdl.groupId of
+            Nothing ->
+                []
+
+            Just gid ->
+                [ title
+                , inst
+                , groupId gid
+                , cancelCheckin
+                , checkInTitle
+                , listPersonView Nothing mdl.waitingCheckIn
+                ]
