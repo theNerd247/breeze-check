b0f35f76bd6e0bcf1f43e1ce944597e89711e86d
theNerd247 <noah.harvey247 gm>
2018-03-12 23:24:49 -0400

    ...forgot the photo page...

--- ui/src/Pages/PhotoPage.elm
+++ ui/src/Pages/PhotoPage.elm
@@ -1,28 +1,24 @@
 module Pages.PhotoPage exposing (..)
 
-import Bootstrap.Button as Button
 import Bootstrap.Grid as Grid
 import Bootstrap.Grid.Col as Col
 import Bootstrap.Grid.Row as Row
 import FindPeople as Find
 import Html as Html exposing (Html, div, h1, h2, h3, h4, p, text)
-import Person as Person
+import Pages exposing (..)
+import Router as Router
 
 
-type alias Msg =
-    Find.Msg
+config : Config
+config =
+    { pageRoute = Router.Photo
+    , nextPageRoute = Router.Safety
+    , pageTitle = "Photo Waiver"
+    , pageView = view
+    }
 
 
-type alias HasPhotoPage m =
-    Find.HasFind m
-
-
-update : Msg -> HasPhotoPage m -> ( HasPhotoPage m, Cmd Msg )
-update =
-    Find.update
-
-
-view : HasPhotoPage m -> Html Msg
+view : Model -> Html Msg
 view mdl =
     div []
         [ Grid.row [ Row.centerXs ]
@@ -44,25 +40,11 @@ view mdl =
             ]
         , Grid.row [ Row.centerXs ]
             [ Grid.col [ Col.xsAuto ]
-                [ Html.map Find.EditWaitingMsg <|
-                    Person.view mdl.waitingCheckIn
-                        Person.selectPersonsForWantsPhotos
-                ]
+                [ Html.map FindMsg <| Find.waitingPersonsWithPhotoSelect mdl ]
             ]
         , Grid.row [ Row.centerXs ]
             [ Grid.col [ Col.xsAuto ]
-                [ checkInButton
+                [ continueButton False [ text "Contine" ]
                 ]
             ]
-        ]
-
-
-checkInButton : Html Msg
-checkInButton =
-    Button.button
-        [ Button.success
-        , Button.block
-        , Button.onClick Find.CheckInClick
-        ]
-        [ text "Check In "
         ]
